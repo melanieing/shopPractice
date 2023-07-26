671958c4429a2fb1268595e97cf8bf285623d2f7
melanieing <melanie0617@gmail.com>
2023-07-26 16:32:47 +0900

    first setting

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static ConcurrentHashMap<Long, Member> store = new ConcurrentHashMap<>(); // 동시성 이슈 때문에 사용
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
