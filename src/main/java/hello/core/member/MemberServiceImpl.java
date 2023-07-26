a0a997d0cdf5c6b09aa3ed9a42ca6ba9b48f2ce4
melanieing <melanie0617@gmail.com>
2023-07-26 18:02:50 +0900

    주문, 할인 만들기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,6 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
-
     @Override
     public void join(Member member) {
         memberRepository.save(member);
---
+++ src/main/java/hello/core/order/Order.java
@@ -0,0 +1,8 @@
+package hello.core.order;
+
+public class Order {
+    private Long memberId;
+    private String itemName;
+    private int itemPrice;
+    private int discountPrice;
+}
