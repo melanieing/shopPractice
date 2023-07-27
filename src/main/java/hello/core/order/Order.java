0a3f2ba6bfe0549e74f2e1ced5461f261fdb2460
melanieing <melanie0617@gmail.com>
2023-07-27 13:30:37 +0900

    주문과 할인 테스트

--- src/main/java/hello/core/order/Order.java
+++ src/main/java/hello/core/order/Order.java
@@ -5,4 +5,41 @@ public class Order {
     private String itemName;
     private int itemPrice;
     private int discountPrice;
+
+    public Order(Long memberId, String itemName, int itemPrice, int discountPrice) {
+        this.memberId = memberId;
+        this.itemName = itemName;
+        this.itemPrice = itemPrice;
+        this.discountPrice = discountPrice;
+    }
+
+    public int calculatePrice() {
+        return itemPrice - discountPrice;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getItemPrice() {
+        return itemPrice;
+    }
+
+    public int getDiscountPrice() {
+        return discountPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "memberId=" + memberId +
+                ", itemName='" + itemName + '\'' +
+                ", itemPrice=" + itemPrice +
+                ", discountPrice=" + discountPrice +
+                '}';
+    }
 }
