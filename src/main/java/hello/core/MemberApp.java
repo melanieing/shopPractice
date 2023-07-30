7fb8bd61efc0b847483dfa98834494dcd97b750d
melanieing <melanie0617@gmail.com>
2023-07-30 20:08:57 +0900

    ApplicationContext 사용하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -11,7 +11,7 @@ public class MemberApp {
 //        MemberService memberService = appConfig.memberService();
 
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        applicationContext.getBean()
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
